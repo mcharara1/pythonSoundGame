66fbdcbbf53b255e4c1b96a162b5cc96cdaeac4f
mcharara1 <mcharara@vols.utk.edu>
2026-05-03 18:27:33 -0400

    mohsen: set up main and added some graphics.

---
+++ bar.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+import random
+import obstacle1
+import time
+import sounddevice as sd
+import numpy as np
+#here is where we should make a sound level checker and adjust bar image based on the level. create a threshold for each level.
+#not required: a great feature that we could add is if the threshold can be adjusted with just one variable.
+#create array that stores thresholds and maybe applies the multiplier to them, which are then used in a big if else else else statement.
+# the if statement returns the propper bar image, and also would initiate the propper obstacle defeat sequence. 
+
+
+#find out how to udjust this number:        ↓     from 0-10.
+barGraphic = pygame.image.load('graphics/bar7.png')
+
