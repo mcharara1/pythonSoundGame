9f123503bb2c90511836cff72fbce28f50c9004a
pwilso33 <pwilso33@vols.utk.edu>
2026-05-05 11:00:00 -0400

    a

--- Background.py
+++ Background.py
@@ -14,6 +14,6 @@ pygame.init()
 
 bgroundOrig = pygame.image.load('graphics/Background.png')
 orig_width, orig_height = bgroundOrig.get_size()
-scale_factor = 3
+scale_factor = 1.2
 new_size = (int(orig_width * scale_factor), int(orig_height * scale_factor))
 bground = pygame.transform.scale(bgroundOrig, new_size)
