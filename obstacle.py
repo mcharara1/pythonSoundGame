bf944737f9a973e9d1672eb3d9b471eae0c1af2a
mcharara1 <mcharara@vols.utk.edu>
2026-05-05 09:32:22 -0400

    test

--- obstacle.py
+++ obstacle.py
@@ -1,1 +1,18 @@
- 
+import pygame
+import sys
+import random
+import time
+import sounddevice as sd
+import numpy as np
+import bar
+import Runner
+
+pygame.init()
+#this here adjusts the size of the sprite. would change or get rid of this if we change the sprite
+slimeOrig = pygame.image.load('graphics/slime.png') 
+orig_width, orig_height = slimeOrig.get_size()
+scale_factor = 3  # increase size by 300%
+new_size = (int(orig_width * scale_factor), int(orig_height * scale_factor))
+slime = pygame.transform.scale(slimeOrig, new_size)
+
+#here we gotta make a transform position function so it looks like the dog is running to the obstacle, and also a detect collision function
